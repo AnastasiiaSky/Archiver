a02a188b4c4ea37bce2deecf789268f17513906b
Qyle Nettie <qylenett@at-b2.msk.21-school.ru>
2024-03-18 15:28:00 +0300

    Some methods were added

--- Archiver/src/main/java/org/gaposha/command/ZipContentCommand.java
+++ Archiver/src/main/java/org/gaposha/command/ZipContentCommand.java
@@ -1,8 +1,21 @@
 package org.gaposha.command;
 
+import org.gaposha.ConsoleHelper;
+import org.gaposha.FileProperties;
+import org.gaposha.ZipFileManager;
+
+import java.util.List;
+
 public class ZipContentCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
-
+        ConsoleHelper.writeMessage("Просмотр содержимого архива.");
+        ZipFileManager zipFileManager = getZipFileManager();
+        ConsoleHelper.writeMessage("Содержимое архива:");
+        List<FileProperties> properties = zipFileManager.getFilesList();
+        for (int i = 0; i < properties.size(); i++) {
+            ConsoleHelper.writeMessage(properties.get(i).toString());
+        }
+        ConsoleHelper.writeMessage("Содержимое архива прочитано.");
     }
 }
--- Archiver/src/main/java/org/gaposha/command/ZipRemoveCommand.java
+++ Archiver/src/main/java/org/gaposha/command/ZipRemoveCommand.java
@@ -1,8 +1,22 @@
 package org.gaposha.command;
 
+import org.gaposha.ConsoleHelper;
+import org.gaposha.ZipFileManager;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class ZipRemoveCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
+        ConsoleHelper.writeMessage("Удаление файла из архива.");
 
+        ZipFileManager zipFileManager = getZipFileManager();
+
+        ConsoleHelper.writeMessage("Введите полный путь файла в архиве:");
+        Path sourcePath = Paths.get(ConsoleHelper.readString());
+        zipFileManager.removeFile(sourcePath);
+
+        ConsoleHelper.writeMessage("Удаление из архива завершено.");
     }
 }
